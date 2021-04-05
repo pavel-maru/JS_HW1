0e3de0da198e74c583f13fe060b8e78c56e58e92
pavel-maru <71653912+pavel-maru@users.noreply.github.com>
2021-04-05 22:39:24 +0300

    first commit

--- task_1_2.js
+++ task_1_2.js
@@ -1,8 +1,10 @@
 
 // 2. Работа с переменными. Объявить две переменные: admin и name. Записать в name строку "Василий"; Скопировать значение из name в admin. Вывести admin (должно вывести «Василий»).
 
-let admin
-let name
-name = "Василий"
-admin = name
-alert('admin: ' + admin)
+"use strict";
+
+let admin;
+let name;
+name = "Василий";
+admin = name;
+alert('admin: ' + admin);
